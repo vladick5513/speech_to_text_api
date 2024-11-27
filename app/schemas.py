1629c22e1448ad9458af80496653ddfb1bcea8e8
vladick5513 <2006krickunow@gmail.com>
2024-11-27 16:22:24 +0300

    update schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,6 @@
 from pydantic import BaseModel
 
 
-class DialogPart(BaseModel):
-    text: str
-    duration: float
-    raised_voice: bool
-    gender: str
-
-
 class ASRResponse(BaseModel):
     dialog: dict
     result_duration: dict
